3da8f2042ee02f0d4382d9751d349969d413453c
欧麟轩 <lxmm@oulinxuandeMacBook-Pro.local>
2017-03-06 09:35:14 +0800

    Initial Commit

---
+++ 门诊工作/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  门诊工作
+//
+//  Created by Xuan on 2017/3/6.
+//  Copyright © 2017年 Xuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
